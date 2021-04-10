3ac8cfad338e3762fe61f76ec078855e83113c99
Ryuya-couchpotato <61501141+Ryuya-couchpotato@users.noreply.github.com>
2021-04-10 13:15:49 +0900

    update

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//1,パネルを入れ替える(表示中のパネルをdisplay:noneにし、引数のパネルをvisibleにする)
+//
+
+
+/*
+CSSクラスの削除、追加の方法
+const button = document.querySelector('.primary')
+button.classList.remove('primary')
+button.classList.add('secondary')
+*/
